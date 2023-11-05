1b0af40b9aff9bfea2a02e5471b69c0433b62e2d
Rushikesh Patil <69971167+patil-rushikesh@users.noreply.github.com>
2023-11-05 21:39:30 +0530

    Update selectionsort2.c
    Made some changes in this code..!
    Check it out

--- selectionsort2.c
+++ selectionsort2.c
@@ -1,30 +1,37 @@
-#include<stdio.h>
-int main(){
-    int n,i,j,min,temp;
-    printf("enter size of an array");
-    scanf("%d",&n);
+#include <stdio.h>
+
+int main() {
+    int n, i, j, min, temp;
+    printf("Enter the size of an array: ");
+    scanf("%d", &n);
     int a[n];
-    //enter array elements
-    for(i=0;i<n;i++){
-        printf("enter number%d",i+1);
-        scanf("%d",&a[i]);
+
+    // Enter array elements
+    for (i = 0; i < n; i++) {
+        printf("Enter number %d: ", i + 1);
+        scanf("%d", &a[i]);
     }
-    //elements need to be sorted
-    for(i=0;i<n-1;i++){
-        min=i;
-        for(j=i+1;j>n;j++){
-            if(a[j]<a[min]){
-                min=j;
+
+    // Elements need to be sorted
+    for (i = 0; i < n - 1; i++) {
+        min = i;
+        for (j = i + 1; j < n; j++) { // Change '>' to '<' here
+            if (a[j] < a[min]) {
+                min = j;
             }
         }
-        //swap a[i] and a[min]
-     temp=a[i];
-    a[i]=a[min];
-    a[min]=temp;
+        // Swap a[i] and a[min]
+        temp = a[i];
+        a[i] = a[min];
+        a[min] = temp;
     }
-    //sorted array is
-    for(i=0;i<n;i++){
-        printf("%d",a[i]);
+
+    // Sorted array is
+    printf("Sorted array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", a[i]); // Added space to separate elements
     }
+    printf("\n");
+
     return 0;
 }
